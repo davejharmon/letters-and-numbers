379f7cf51e0d7775de6458e6c928ad0735a18597
davejharmon <davejharmon@gmail.com>
2021-12-04 01:24:41 +1100

    Working on model.crunchNumbers()

--- js/model.js
+++ js/model.js
@@ -9,7 +9,7 @@ export const state = {
       picks: [],
       stack1: [],
       stack2: [],
-      target: 101,
+      target: [],
       maxLength: 0,
       time: 0,
       countdown: false,
@@ -48,14 +48,17 @@ export const newGame = function (gametype) {
     stack1: set1,
     stack2: set2,
     target: [],
+    solution: '',
     maxLength: maxLength,
     time: +CONFIG.TIMER_SEC,
     countdown: false,
   };
 
   // if numbers round, populate targetNums array
-  for (let i = 0; i < 7; i++) {
-    newRound.target.push(Math.floor(Math.random() * 900) + 100);
+  if (gametype === 'numbers') {
+    for (let i = 0; i < 7; i++) {
+      newRound.target.push(Math.floor(Math.random() * 900) + 100);
+    }
   }
 
   //update the state
@@ -68,6 +71,7 @@ export const newGame = function (gametype) {
 export const findSolution = async function (round) {
   // calculate answer here
   console.log("Let's find a solution...");
+  if (round.type === 'numbers') this.crunchNumbers(round);
   // const apiAddress = `http://www.anagramica.com/best/idnetity`;
   // const result = await AJAX(apiAddress);
   // console.log(result);
@@ -80,4 +84,53 @@ export const pickNumber = function (stackNum) {
   if (!num) throw new Error('no numbers left bazza');
   state.game[state.round].picks.push(num);
   return num;
+};
+
+export const crunchNumbers = function (round) {
+  console.log("Let's find a numbers solution...");
+  // step one: shuffle the picks into a new array
+  const shuffledPicks = [...round.picks].sort(() => Math.random() - 0.5);
+  let num;
+  console.log(shuffledPicks);
+
+  // step three: loop through the array attempting a random legal operation in order of complexity
+
+  // TO DO: Start by multiplying one big by one small?
+  const expr = shuffledPicks.reduce(
+    (acc, pick) => {
+      const opKey = Math.floor(Math.random() * 4);
+      console.log(`Next number is ${pick}...`);
+      console.log(acc);
+
+      switch (opKey) {
+        case 0:
+          console.log("Let's try division first");
+          num = acc[0] / pick;
+
+          if (num % 0 && num > 100) return [acc[0] / pick, `${acc[1]}/${pick}`];
+
+        case 1:
+          console.log("Let's try multiplication next.");
+          num = acc[0] * pick;
+
+          if (num > 0 && num < 1000)
+            return [acc[0] * pick, `${acc[1]}*${pick}`];
+
+        case 2:
+          console.log("Let's try subtraction third.");
+          num = acc[0] - pick;
+
+          if (num >= 100) return [acc[0] - pick, `${acc[1]}-${pick}`];
+
+        case 3:
+          console.log("OK fine, we'll add it.");
+          return [acc[0] + pick, `${acc[1]}+${pick}`];
+      }
+    },
+    [0, '']
+  );
+  console.log('All done!');
+  console.log(expr);
+  round.target.push(expr[0]);
+  round.solution = expr[1];
 };
--- js/views/buttonsView.js
+++ js/views/buttonsView.js
@@ -16,13 +16,18 @@ class ButtonsView extends View {
   _generateMarkup() {
     return `
     <div class="game-buttons">
-      <button class="btn" id="game-btn-1" data-action="1">BIG NUMBER</button>
+      <button class="btn" id="game-btn-1" data-action="1">${
+        this._data.type == 'numbers' ? 'BIG NUMBER' : 'VOWEL'
+      }</button>
       <button class="btn timer 
       ${this._data.countdown ? '' : 'hidden'}
       " id="game-btn-timer" data-action="timer">
       ${this._data.countdown ? this._data.time : '⏱ TIMER'}
       </button>
-      <button class="btn" id="game-btn-2"data-action="2">SMALL NUMBER</button>
+      <button class="btn" id="game-btn-2"data-action="2">${
+        this._data.type == 'numbers' ? 'SMALL NUMBER' : 'CONSONANT'
+      }
+      </button>
     </div>`;
   }
 }
